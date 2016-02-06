45e6c7bf1ff290b0e59b901d8f502bcc578513de
neelrakholia <neel.rakholia@gmail.com>
2016-02-06 10:02:06 -0500

    Added more general features and js functionality

--- python/webpage_pull.py
+++ python/webpage_pull.py
@@ -5,55 +5,57 @@ from bs4 import BeautifulSoup as BSHTML
 from textblob import TextBlob
 from HTMLParser import HTMLParser
 import sys
+import urllib
+from cookielib import CookieJar
+from textblob.sentiments import NaiveBayesAnalyzer
+from pattern.en import sentiment
+import cgi
+form = cgi.FieldStorage()
 
-# create a subclass and override the handler methods
-class MyHTMLParser(HTMLParser):
-    def handle_starttag(self, tag, attrs):
-        print ""
-    def handle_endtag(self, tag):
-        print ""
-    def handle_data(self, data):
-        print data
+cj = CookieJar()
+opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
 
 # define a function for converting none strings to empty strings
 def xstr(s):
     if s is None:
         return ''
-    return str(s)
+    re.sub('[^a-zA-Z0-9-_*.]', '', s)
+    s = s.encode('ascii','ignore')
+    return s
 
 # read html from website
-req = urllib2.Request(sys.argv[1])
-response = urllib2.urlopen(req)
+req = urllib2.Request(form["url"], headers={'User-Agent' : "Magic Browser"})
+response = opener.open(req)
 the_page = response.read()
 
 # get headers from page and perfrom sentiment analysis on them
 # parse page
 BS = BSHTML(the_page)
 i = 0
-polarity_sum = 0
-subjectivity_sum = 0
+p_sum = 0
+n_sum = 0
 # search for paragraph
 for text in BS.find_all('p'):
     # find the string in the text
     for child in text.descendants:
-        string = child
+        string = child.string
 
     # process text
+    string = xstr(string)
     string = string.lower()
     string = string.split()
     string = " ".join(string)
     string = string.strip()
-    re.sub('[^a-zA-Z0-9-_*.]', '', string)
+    string = string.encode('ascii','ignore')
     
     # calculate sentiment
     if len(string.split()) > 0:
-        string = string.encode('ascii','ignore')
-        blob = TextBlob(string)
-        polarity_sum += blob.sentiment.polarity
-        subjectivity_sum += blob.sentiment.subjectivity
+        blob = sentiment(string, analyzer=NaiveBayesAnalyzer())
+        p_sum += blob[0]
+        n_sum += blob[1]
         i += 1
        
 
 # print sentiment values
-print "Polarity:     ", polarity_sum/i
-print "Subjectivity: ", subjectivity_sum/i
+print "Polarity:     ", p_sum/i
+print "Subjectivity: ", n_sum/i
